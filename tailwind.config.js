8abaf26f42f235e6461892c5edd79bfb6364c4c8
pablozamit <166626152+pablozamit@users.noreply.github.com>
2025-08-28 17:17:18 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Le dice a Tailwind que escanee todos tus componentes de React
+  ],
   theme: {
-    extend: {},
+    extend: {
+      // Aquí puedes añadir tus colores neón personalizados para usarlos directamente en las clases
+      // Ejemplo: className="text-neon-blue"
+      colors: {
+        'neon-blue': '#00f3ff',
+        'neon-purple': '#bf00ff',
+        'neon-green': '#39ff14',
+        'neon-gold': '#ffd700',
+      }
+    },
   },
   plugins: [],
 }
-
